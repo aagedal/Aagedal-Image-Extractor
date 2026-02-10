81b066a57aa2f6efd9a321da4cedd160528cf492
Truls Aagedal <5827165+aagedal@users.noreply.github.com>
2026-02-10 14:23:07 +0100

    Initial Commit

---
+++ Aagedal Image Extractor/Aagedal_Image_ExtractorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aagedal_Image_ExtractorApp.swift
+//  Aagedal Image Extractor
+//
+//  Created by Truls Aagedal on 10/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct Aagedal_Image_ExtractorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
